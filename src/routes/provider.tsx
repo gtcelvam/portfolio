dd986ff4d518856b33d5c1486a6126fc21c8ba48
gtcelvam <selvasizzler@gmail.com>
2023-04-26 23:09:13 +0530

    Apollo query updated.

--- src/routes/provider.tsx
+++ src/routes/provider.tsx
@@ -1,19 +1,17 @@
 import React, { FC, ReactElement } from "react";
 import { Provider as ToolkitProvider } from "react-redux";
-import {ApolloClient,InMemoryCache,ApolloProvider} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import store from "../utils/slice/configureStore";
 
 type ProviderPropsType = { children: ReactElement };
 
 const Provider: FC<ProviderPropsType> = ({ children }) => {
-
   //constants
   //Client will be connected here
-  const client:any = new ApolloClient({
+  const client: any = new ApolloClient({
     uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
   });
-
 
   return (
     <ApolloProvider client={client}>
